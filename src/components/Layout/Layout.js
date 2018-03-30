f7e586ecbef293fd5140734388701dd0b9b98c9b
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-03-30 22:32:40 +0800

    Burger basic structure, hardcoded

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxx';
 
+import classes from './Layout.css';
+
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDraw, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
